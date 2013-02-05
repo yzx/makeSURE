70cc8006ec2d1b420e3b3e47dc9a101676c20281
piotr <piotr.goszczynski@gmail.com>
2013-02-04 23:00:08 -0500

    Compilation time defines added

--- keil/def.mk
+++ keil/def.mk
@@ -13,25 +13,33 @@ MPX=     map
 APX=     OMF
 OHX=     ihx
 
-ifeq ($(CONFIG),Debug)
-AS_BD_FLAG= DB
-CC_BD_FLAG= OT"(2, Size)"
+
+ifneq ($(CONFIG),Release)
+# Debug and all others
+CONFIG_AS_FLAGS= DB
+CONFIG_DEF?= DEBUG
+CONFIG_CC_FLAGS=  OT"(2, Size)"
+CONFIG_LD_FLAGS=
 else
-AS_BD_FLAG= DF"(NDEBUG)"
-CC_BD_FLAG= OT"(8, Size)" DF"(NDEBUG)"
+CONFIG_AS_FLAGS=
+CONFIG_DEF?= NDEBUG
+CONFIG_CC_FLAGS=  OT"(8, Size)"
+CONFIG_LD_FLAGS=
 endif
 
-SLFILES=  $(addprefix $(IDIR),$(addsuffix .$(SLX),$(SLFILE)))
+ifneq ($(CCDEF) $(CONFIG_DEF),)
+CCDEFS= DF"($(subst $(space),$(SPACE), $(call inject-comma,$(CCDEF) $(CONFIG_DEF) )))"
+endif
 
 ifneq ($(IDIR),)
-INCDIR= INCDIR"($(subst $(space),$(SPACE), $(call inject-comma,$(IDIR))))"
+IDIRS= INCDIR"($(subst $(space),$(SPACE), $(call inject-comma,$(IDIR))))"
 endif
 
-TCFLAGS= PR"($(DIRT)/"$*".$(LSX))" $(INCDIR) OJ"("$@")"
+TCFLAGS= PR"($(DIRT)/"$*".$(LSX))" $(IDIRS) $(CCDEFS) OJ"("$@")"
 
-ASFLAGS= NOCO MACRO CASE EP $(AS_DB_FLAG) $(TCFLAGS)
+ASFLAGS= NOCO MACRO CASE EP $(CONFIG_AS_FLAGS) $(TCFLAGS)
 
-CCFLAGS= NOIV LARGE MODDP2 OMF2 CD DB NOIP VB"(1)" $(CC_DB_FLAG) $(TCFLAGS)
+CCFLAGS= NOIV LARGE MODDP2 OMF2 CD DB NOIP VB"(1)" $(CONFIG_CC_FLAGS) $(TCFLAGS)
 
 LDFLAGS= PR"($(DIRT_PREFIX)/$(NAME).$(MPX))" IX CODE DW"(16)" RU \
 CL"(XDATA(X:0x0000-X:0x1FFF))" \
